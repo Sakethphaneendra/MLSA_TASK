42483ea9e2b57ae916f43a257dc2a0858ce5bcf5
unknown <sakethphaneendra@gmail.com>
2023-10-17 02:31:41 +0530

    First Coomit

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Saketh Phaneendra Kondisetti",
+  title: "Software Engineer  ",
+  email: "SakethPhaneendra.Kondisetti@studentambassadors.com",
+  gitHub: "Sakethphaneendra",
+  instagram: "",
+  linkedIn: "sakethphaneendra",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
